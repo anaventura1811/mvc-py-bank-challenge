ff01899bcc64f43ce91218f043f67cd281a5387f
Ana Ventura <anaventura1811@gmail.com>
2024-11-07 10:39:14 -0300

    feat: update requirements.txt file, add env example file, implement db connection and integration test

---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,12 @@
+# import pytest
+from sqlalchemy.engine import Engine
+from .connection import database_connection_handler
+
+
+def test_connection_to_database():
+    assert database_connection_handler.get_engine() is None
+    database_connection_handler.connect_to_db()
+    db_engine = database_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
